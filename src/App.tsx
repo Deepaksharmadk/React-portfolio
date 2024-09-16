43ad1bf49db9e875d2d570fc561d766c684e899d
deepaksharmadk <dks847203@gmail.com>
2024-09-17 02:36:23 +0530

    setup  vite and shadcn Ui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import About from './components/About/About';
+import Header from './components/Header/Header';
+import SkillSection from './components/Skill/Skills';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <About />
+      <SkillSection />
+    </div>
+  );
+}
+
+export default App;
